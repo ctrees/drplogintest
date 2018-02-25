c8876d5a2bb5730c2e5449aa11f8271ad91ba7fd
Mail Services Operations <msops@catmini.cat9.private>
2018-02-25 09:50:31 -0600

    cleanup

--- test/Login.page.js
+++ test/Login.page.js
@@ -2,7 +2,23 @@ class Login {
   get email () { return $('#username'); }
   get password () { return $('#password'); }
   get submit () { return $('input.btn.btn-primary.submitButton-customizable'); }
-  
+
+  get racknSupportLogoutinButton () { return $('#header > div:nth-child(4) > a > a > div > div.visible.content'); } 
+  get racknCurrentUserButton () { return $('#header > div:nth-child(3) > div > div > div.text'); }
+  get racknUserLogoutButton () { return $('#header > div:nth-child(3) > div > div > div.text'); }
+
+  gotoRackNPortal (portalText) {
+    var portalButtonElement = this.racknSupportLogoutinButton;
+    var failMessage = "Waited 5s and did not see: " + portalText;
+    console.log("Before Check: ", portalButtonElement.getText());
+    /*
+    browser.waitUntil(function() {
+      return portalButtonElement.getText() == portalText
+    }, 5000, failMessage );
+    */
+    console.log("After Check: ", portalButtonElement.getText());
+  }
+
   login (email, password) {
     this.email.setValue(email);
     this.password.setValue(password);
@@ -10,16 +26,37 @@ class Login {
     this.submit.click();
   }
 
-  isCurrentUser (email) {
-    var currentUser;
+  logout (email, logoutbuttontext) {
+    var logoutSupportButtonElement = this.racknSupportLogoutinButton;
+    var logoutUserButtonElement = this.racknUserLogoutButton;
 
-    // Wait around for Fancy Floaty iconly jasony fetchy poo to settle down
     browser.waitUntil(function() {
-      return browser.getText('#header > div:nth-child(3) > div > div > div.text') === email
+      return logoutButtonElement.getText() == logoutbuttontext
+    }, 5000, 'expect text to be _Support & Logout_ after 5s');
+
+    this.racknloginoutbutton.click();
+
+    browser.waitUntil(function() {
+      return logoutButtonElement.getText() == logoutbuttontext
+    }, 5000, 'expect text to be _Support & Logout_ after 5s');
+
+  }
+
+  getCurrentUser () {
+    var currentUserElement = this.racknCurrentUserButton;
+  //  var returnText = currentUserElement.getText();
+    var returnText = "Hello-getCurrentUser"
+    return returnText;
+  }
+
+  isCurrentUser (email) {
+    var currentUserElement = this.racknCurrentUserButton;
+    console.log("Before Check: ", currentUserElement.getText());
+    browser.waitUntil(function() {
+      return currentUserElement.getText() == email
     }, 5000, 'expect text to be currentUser after 5s');
-    
-    currentUser = browser.getText('#header > div:nth-child(3) > div > div > div.text');
-    return currentUser == email;
+    console.log("After Check: ", currentUserElement.getText());
+    return currentUserElement.getText() == email;
   }
 
   isLoggedIn () {
--- test/login.spec.js
+++ test/login.spec.js
@@ -1,21 +1,54 @@
 const Login = require('./Login.page.js');
-
 const login = new Login();
 
+//-BEGIN promise rejection hacks
+//-via http://thecodebarbarian.com/unhandled-promise-rejections-in-node.js.html
+//const obj = { fn: () => {} };
+
+//-via https://github.com/mochajs/mocha/issues/2640
+let unhandledRejectionExitCode = 0;
+
+process.on("unhandledRejection", (reason) => {
+	console.log("unhandled rejection:", reason);
+	unhandledRejectionExitCode = 1;
+	throw reason;
+});
+
+process.prependListener("exit", (code) => {
+	if (code === 0) {
+		process.exit(unhandledRejectionExitCode);
+	}
+});
+//-END promise rejection hacks
 
 describe('Login Page', function () {
   const validEmail = 'test@horseoff.com';
   const validPass = 'GoodUser1';
+  const loginText = 'RackN Portal Login'
+  const logoutText = 'Logout & Support';
 
   beforeEach(function() {
+
     var racknloginbutton = '#header > div:nth-child(4) > a > a > div.visible.content';
+    var expectedLoginText = 'RackN Portal Login';
     // Go to baseurl from wdio.conf.js
     browser.url('./');
+    //login.gotoRackNPortal(expectedLoginText);
     // Click the racknlogin button to pick up a token
     browser.click(racknloginbutton);
     // Now we can use login page object Login.page.js
-
+    //- Some debugging aids
     //browser.debug();
+    //browser.pause(10000);
+  });
+
+  afterEach(function() {
+
+    var currentUserText = login.getCurrentUser();
+    console.log('afterEach');
+    console.log(currentUserText);
+    //expect(login.isCurrentUser(validEmail), 'Valid User logged on').to.be.true;
+    //login.logout(validEmail, logoutText);
   });
 
   it('should maintain a consistant Login portal', function () {
@@ -26,6 +59,7 @@ describe('Login Page', function () {
     results.forEach(function (result) {
       expect(result.isWithinMisMatchTolerance).to.equal(true, 'screenshot failure');
     });
+
   })
   
   it('should let you login with valid credentials', function () {
